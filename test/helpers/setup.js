783d0adbd3f06b179a0ddfc7542e0d525e35871c
Hector Flores <hecto932@gmail.com>
2014-09-21 19:53:45 -0430

    Construyendo una API rest con Node.js

---
+++ test/helpers/setup.js
@@ -0,0 +1,7 @@
+/**
+ * Assertion and testing utilities
+ */
+var chai = require('chai');
+
+GLOBAL.AssertionError = chai.AssertionError;
+GLOBAL.expect = chai.expect;
---
+++ test/notas.js
@@ -0,0 +1,47 @@
+var request = require('supertest');
+var api = require('../server.js');
+var host = process.env.API_TEST_HOST || api;
+
+request = request(host);
+
+describe('Coleccion de Notas [/notas]', function() {
+
+  describe('POST', function() {
+    it('deberia crear una nota', function(done) {
+      var data = {
+        "nota": {
+          "title": "Mejorando.la #node-pro",
+          "description": "Introduccion a clase",
+          "type": "js",
+          "body": "soy el cuerpo de json"
+        }
+      };
+
+      request
+        .post('/notas')
+        .set('Accept', 'application/json')
+        .send(data)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+        .end(function(err, res) {
+          var nota;
+          var body = res.body;
+          console.log('body', body);
+
+          // Nota existe
+          expect(body).to.have.property('nota');
+          nota = body.nota;
+
+          // Propiedades
+          expect(nota).to.have.property('title', 'Mejorando.la #node-pro');
+          expect(nota).to.have.property('description', 'Introduccion a clase');
+          expect(nota).to.have.property('type', 'js');
+          expect(nota).to.have.property('body', 'soy el cuerpo de json');
+          expect(nota).to.have.property('id');
+
+          done(err);
+        });
+    });
+  });
+
+});
