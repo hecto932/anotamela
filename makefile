b1f031873dfc1196360cf1997af102c151d6ec7d
Hector Jose Flores Colmenarez <hecto932@gmail.com>
2014-09-23 15:27:42 -0430

    Proyecto desde 0

--- makefile
+++
@@ -1,9 +0,0 @@
-DEFAULT:
-	make test
-
-test:
-	./node_modules/.bin/mocha $(ARGS) test/
-
-.PHONY: \
-	DEFAULT \
-	test \
--- test/helpers/setup.js
+++
@@ -1,7 +0,0 @@
-/**
- * Assertion and testing utilities
- */
-var chai = require('chai');
-
-GLOBAL.AssertionError = chai.AssertionError;
-GLOBAL.expect = chai.expect;
