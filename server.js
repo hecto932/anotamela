bc68a94dfdabef91101b536595fe2469dd8b177c
Hector Jose Flores Colmenarez <hecto932@gmail.com>
2014-09-24 12:40:49 -0430

    Mitad de la segunda clase, error post

--- server.js
+++ server.js
@@ -1,9 +1,41 @@
+//Dependencies
 var express = require('express');
+var bodyParser = require('body-parser');
+//Local Variables
+var server = express();
 
-var app = module.exports = express();
+//MIDDLEWARE
+server.use(bodyParser.json('application/json'));
 
-/*
-app.listen(3000, function(){
-	console.log('Hola anotamela desde http://localhost:3000/');
+server.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//Routes
+server.post('/notas', function(req, res){
+
+	console.log('POST', req);
+	
+	res
+		.status(201)
+		.json(req.body);
 });
-*/
+
+server.post('/usuario', function(req, res){
+	console.log("POST", req.body);
+	res
+		.status(201)
+		.json({"Usuario":"exitoso"});
+});
+
+//Expose or start server
+if(!module.parent)
+{
+	server.listen(3000, function(){
+		console.log("Hola estoy escuchando en el puerto 3000 en http://localhost:3000/");
+	});
+}
+else
+{
+	module.exports = server;
+}
--- test/notas.js
+++ test/notas.js
@@ -6,39 +6,46 @@ request = request(host);
 
 describe('recurso /notas', function(){
 	describe('POST', function(){
-		it("deberia de crear una nota nueva", function(){
-			//throw new Error('Tengo hambre');
-			//return true;
+    it("deberia de crear una nota nueva", function(done){
+      var data = {
+        "title": "Mejorando.la #node-pro",
+        "description": "Introduccion a clase",
+        "type": "js",
+        "body": "soy el cuerpo de json"
+      };
 
-			var data = {
-        		"nota": {
-          			"title": "Mejorando.la #node-pro",
-          			"description": "Introduccion a clase",
-          			"type": "js",
-          			"body": "soy el cuerpo de json"
-        		}
-      		};
+      // crear solicitud de http enviando data
+      request
+        .post('/notas')
+        // Accept application/json
+        .set('Accept', 'application/json')
+        // Status Code = 201
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+        .end(function(err, res){
+          var body = res.body;
 
-      		request
-      			.post('/notas')
-      			.set('Accept', 'application/json')
-      			.send(data)
-      			.expect(201)
-      			.expect('Content-Type', /application\/json/)
-      			.end(function(err, res){
-      				var body = res.body;
+          expect(body).to.have.property('nota');
 
-      				expect(body).to.have.property('nota');
+          nota = body.notas;
 
-      				var nota = body.nota;
+          expect(body).to.have.property('title', 'Mejorando.la #node-pro');
+          expect(body).to.have.property('description', 'Introduccion a clase');
+          expect(body).to.have.property('type', 'js');
+          expect(body).to.have.property('body', 'soy el cuerpo de json');
+          expect(body).to.have.property('id');
+          done();
 
-      				//Propiedades
-      				expect(nota).to.have.property('title','Mejorando.la #node-pro');
-      				expect(nota).to.have.property('description','Introduccion a clase');
-      				expect(nota).to.have.property('type','js');
-      				expect(nota).to.have.property('body','soy el cuerpo de js');
-      				expect(nota).to.have.property('id');
-      			});
+
+        });
+      // cuerpo de la solicitud debe tenr una nota en JS
+
+      // nota debe tener una propiedad 'title' = "Mejorando.la #node-pro"
+
+
+
+      
+
 		});
 	});
 })
