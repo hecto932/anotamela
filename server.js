04cb27574140444dae65095b30dfcfc773677c49
Hector Jose Flores Colmenarez <hecto932@gmail.com>
2014-09-24 16:45:48 -0430

    Save

--- server.js
+++ server.js
@@ -1,38 +1,28 @@
 //Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
+
 //Local Variables
 var server = express();
 
-//MIDDLEWARE
+//Middleware
 server.use(bodyParser.json('application/json'));
-
-server.use(bodyParser.urlencoded({
-  extended: true
-}));
 
 //Routes
 server.post('/notas', function(req, res){
 
-	console.log('POST', req);
+	console.log('POST', req.body);
 	
 	res
 		.status(201)
 		.json(req.body);
 });
 
-server.post('/usuario', function(req, res){
-	console.log("POST", req.body);
-	res
-		.status(201)
-		.json({"Usuario":"exitoso"});
-});
-
 //Expose or start server
 if(!module.parent)
 {
 	server.listen(3000, function(){
-		console.log("Hola estoy escuchando en el puerto 3000 en http://localhost:3000/");
+		console.log("http://localhost:3000/");
 	});
 }
 else
